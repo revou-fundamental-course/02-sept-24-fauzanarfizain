27e4ae5e7fdb632d9d1f64877bba0826a80ec5bc
fauzanarfizain <fauzanarfizain@gmail.com>
2024-09-11 22:09:06 +0800

    Update JS dan CSS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 // ini Javascript 
+
+// Merubah warna klik menu jajargenjang
+function ccjajargenjang() {
+    document.getElementById('jajar-genjang-btn').style.backgroundColor='bisque';
+    document.getElementById('segitiga-btn').style.backgroundColor='whitesmoke';
+}
+
+// Merubah warna klik menu jajargenjang
+function ccsegitiga() {
+    document.getElementById('jajar-genjang-btn').style.backgroundColor='whitesmoke';
+    document.getElementById('segitiga-btn').style.backgroundColor='bisque';
+}
+
+function validateLuasSegitiga() {
+    let alas=document.getElementById('panjang-alas').value;
+    let tinggi=document.getElementById('tinggi').value;
+
+    if (alas == '' || tinggi == '' || alas.includes('e') || tinggi.includes('e')) {
+        alert('Ada Isian yang Belum Terisi atau Adanya Kesalahan Pengisian Angka. Cek kembali isian Panjang Alas atau Tinggi!')
+    };
+}
+
+function validateKelilingSegitiga() {
+    let sisiA=document.getElementById('sisi-A').value;
+    let sisiB=document.getElementById('sisi-B').value;
+    let sisiC=document.getElementById('sisi-C').value;
+}
+
